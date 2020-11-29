6e9d1a27ef7a3d84cebeff21598e4dd291ac5658
Sanjeet Tiwari <sanjeettiwari00076@gmail.com>
2020-11-29 21:09:43 +0530

    Initialized Vue project

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,29 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div class="app">
+    <Inputs />
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import Inputs from "./components/Inputs.vue";
 
 export default defineComponent({
   name: "App",
   components: {
-    HelloWorld
+    Inputs
   }
 });
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  margin: 0;
+}
+
+.app {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
 }
 </style>
---
+++ src/components/Inputs.vue
@@ -0,0 +1,65 @@
+<template>
+  <form v-on:submit.prevent="addOption()" class="container">
+    <input
+      v-model="option"
+      placeholder="Enter your Option"
+      class="optionInput"
+    />
+    <button type="submit" class="addOption">Add</button>
+  </form>
+</template>
+<script lang="ts">
+import { ref } from "vue";
+export default {
+  name: "Inputs",
+  setup() {
+    const option = ref("");
+    const options = ref<string[]>([]);
+
+    function addOption() {
+      if (option.value) options.value.push(option.value);
+      option.value = "";
+      console.log(options.value);
+    }
+
+    return {
+      option,
+      options,
+      addOption
+    };
+  }
+};
+</script>
+<style lang="scss" scoped>
+.container {
+  background-color: #5c6bc0;
+  padding: 16px;
+  display: flex;
+  justify-content: center;
+}
+
+.optionInput {
+  padding: 5px 16px;
+  font-size: 20px;
+  border: 2px solid white;
+  border-radius: 10px;
+  min-width: 50%;
+  margin-right: 16px;
+}
+
+.addOption {
+  padding: 5px 10px;
+  font-weight: bold;
+  background-color: #3f51b5;
+  border: none;
+  cursor: pointer;
+  text-transform: uppercase;
+  color: #e8eaf6;
+  border-radius: 10px;
+  transition: background-color 0.5s;
+
+  &:hover {
+    background-color: #3949ab;
+  }
+}
+</style>
